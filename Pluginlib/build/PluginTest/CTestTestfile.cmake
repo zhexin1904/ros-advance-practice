138ae81eaad54ca8752518dc04bf76bbebfd3f06
zhexin1904 <15018462039@163.com>
2023-04-25 22:09:17 +0800

    delete build devel

--- Pluginlib/build/PluginTest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jason/ROSlearning/ros-advance-practice/Pluginlib/src/PluginTest
-# Build directory: /home/jason/ROSlearning/ros-advance-practice/Pluginlib/build/PluginTest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Pluginlib/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/jason/ROSlearning/ros-advance-practice/Pluginlib/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/jason/ROSlearning/ros-advance-practice/Pluginlib/devel;/opt/ros/noetic'.split(';') if '/home/jason/ROSlearning/ros-advance-practice/Pluginlib/devel;/opt/ros/noetic' != '' else []
